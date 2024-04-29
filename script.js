543b90784252c966290070bdffbc58300909b9c9
VictorHxHugo <victorzk6@hotmail.com>
2024-04-29 10:25:34 -0300

    Constructor Functions and the new Operator

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+///////////////////////////////
+// Constructor Functions and the new Operator
+
+const Person = function (firstName, birthyear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthyear = birthyear;
+
+  // Never do this
+  // this.calcAge = function () {
+  // console.log(2037- this.birthyear);
+  // };
+};
+
+const victor = new Person('Victor', 1999);
+console.log(victor);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(victor instanceof Person);
